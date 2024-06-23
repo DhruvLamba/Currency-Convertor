02221e19c237f7e871836bc82e95e9636df6e816
Dhruv Lamba <lambadhruv09@gmail.com>
2024-06-24 02:08:09 +0530

    added files

--- currency-convertor/src/hooks/useCurrencyInfo.js
+++ currency-convertor/src/hooks/useCurrencyInfo.js
@@ -4,8 +4,7 @@ function useCurrencyInfo(currency){
     useEffect(()=>{        
         fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
         .then((res)=> res.json() )
-        .then((res)=> setData(res[currency]))
-        
+        .then((res)=> setData(res[currency]))        
     },[currency])
     return data
 }
